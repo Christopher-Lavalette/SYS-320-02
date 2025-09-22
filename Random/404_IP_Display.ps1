c8bc99aafc92e504b02596afe28eb3a63f4d41de
derp-cc <christopher.lavalett@mymail.champlain.edu>
2025-09-22 00:13:19 -0400

    Windows Apache Logs assignment done

---
+++ Random/404_IP_Display.ps1
@@ -0,0 +1,15 @@
+﻿# Get only logs that contain 404, save into $notfounds
+$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String '404'
+
+# Define a regex for IP addresses
+$regex = "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"
+
+# Get $notfounds records that match to the regex
+$ipsunorganized = foreach ($line in $notfounds) { if ($line -match $regex) { $matches[0] } }
+
+# Get IPs as pscustomobject
+$ips = for ($i = 0; $i -lt $ipsunorganized.count; $i++) { 
+    [pscustomobject]@{ IP = $ipsunorganized[$i] } 
+}
+
+$ips
