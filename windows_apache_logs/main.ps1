695e5c7f47260ffe569a31d9c5d78e8bc05eeff7
derp-cc <christopher.lavalett@mymail.champlain.edu>
2025-09-21 23:56:26 -0400

    Windows Apache Logs assignment done

---
+++ windows_apache_logs/main.ps1
@@ -0,0 +1,11 @@
+﻿param (
+    [string]$Page,
+    [string]$HttpCode,
+    [string]$Browser
+)
+
+# Load the function from Apache-Logs.ps1
+. "$PSScriptRoot\Apache-Logs.ps1"
+
+# Call the function with user-supplied inputs
+Get-ApacheLogs -Page $Page -HttpCode $HttpCode -Browser $Browser
